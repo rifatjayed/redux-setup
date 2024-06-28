5d656dd51c0c648aee27a2689f961e280df68b0a
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-06-28 11:09:21 +0600

    redux setup start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button className="px-3 py-3 rounded-md bg-green-500 text-7xl font-semibold">
+          Increment
+        </button>
+        <h1>0</h1>
+        <button className="px-3 py-3 rounded-md bg-red-500 text-xl font-semibold">
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
